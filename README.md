a19fc56a62a0af445dd06f5941566abac5bccd10
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-05-13 16:19:14 -0400

    add readme, license, openwifi.py header

---
+++ README.md
@@ -0,0 +1,18 @@
+openwifi
+========
+
+This script automatically accepts terms and conditions on public wifi so you
+don't have to open a web browser to connect.
+
+Support is currently limited to `attwifi`, but support for additional networks
+will be added in the future.
+
+Prerequisites
+-------------
+* Python 3.x
+* [lxml](http://lxml.de/)
+
+Usage
+-----
+If you are using Arch Linux and netcfg, you can simply run this script as part
+of your `POST_UP` commands. Otherwise, just run this strip after connecting.
--- openwifi.py
+++ openwifi.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+###############################################################################
+# openwifi.py - openwifi
+# Automatically accept terms and conditions on public wifi so you don't have
+# to.
+# Licensed under the ISC License.
+#
+# https://github.com/mutantmonkey/openwifi
+# author: mutantmonkey <mutantmonkey@mutantmonkey.in>
+###############################################################################
 
 from openwifi import attwifi
+
+__author__ = "mutantmonkey <mutantmonkey@mutantmonkey.in>"
+__license__ = "ISC"
 
 if __name__ == '__main__':
     attwifi.AttWifi()
