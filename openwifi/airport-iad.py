7b304b75d6b353feba058a72fa8e4104559d339d
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-05-19 07:53:02 -0400

    add Washington-Dulles International Airport

---
+++ openwifi/airport-iad.py
@@ -0,0 +1,10 @@
+import urllib.request
+import lxml.html
+import openwifi
+
+class AirportIAD(openwifi.Module):
+    def login(self):
+        doc = lxml.html.parse(self.login_page)
+        form = doc.getroot().forms[0]
+        form.fields['terms'] = 'checkbox'
+        lxml.html.submit_form(form)
