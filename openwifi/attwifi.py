a3a112108cd9853f882efe3e2be26c05f703373b
mutantmonkey <mutantmonkey@mutantmonkey.in>
2012-05-19 07:53:19 -0400

    remove extra spacing in attwifi module

--- openwifi/attwifi.py
+++ openwifi/attwifi.py
@@ -6,7 +6,6 @@ class AttWifi(openwifi.Module):
     def login(self):
         doc = lxml.html.parse(self.login_page)
         form = doc.getroot().forms[0]
-
         form.fields['aupAgree'] = 1
         form.fields['connect'] = "Connect"
         lxml.html.submit_form(form)
